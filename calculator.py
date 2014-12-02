53e983de4b370ea1f8b6acb49cbabeb6031f34ca
Charlie Marshall <chas11man@gmail.com>
2014-12-01 22:27:58 -0500

    Functionality done

--- calculator.py
+++ calculator.py
@@ -14,44 +14,63 @@ def calculator():
 
 
 class Add(Resource):
+
     def get(self):
-        a = request.args.get('a')
-        b = request.args.get('b')
-        result = Decimal(a) + Decimal(b)
+        a = Decimal(request.args.get('a'))
+        b = Decimal(request.args.get('b'))
+        result = a + b
         return {'result': float(result)}
+
 
 class Subtract(Resource):
+
     def get(self):
-        a = request.args.get('a')
-        b = request.args.get('b')
-        result = Decimal(a) - Decimal(b)
+        a = Decimal(request.args.get('a'))
+        b = Decimal(request.args.get('b'))
+        result = a - b
         return {'result': float(result)}
+
 
 class Multiply(Resource):
+
     def get(self):
-        a = request.args.get('a')
-        b = request.args.get('b')
-        result = Decimal(a) * Decimal(b)
+        a = Decimal(request.args.get('a'))
+        b = Decimal(request.args.get('b'))
+        result = a * b
         return {'result': float(result)}
+
 
 class Divide(Resource):
+
     def get(self):
-        a = request.args.get('a')
-        b = request.args.get('b')
-        result = Decimal(a) / Decimal(b)
+        a = Decimal(request.args.get('a'))
+        b = Decimal(request.args.get('b'))
+        result = a / b
         return {'result': float(result)}
 
-class Negative(Resource):
+
+class SquareRoot(Resource):
+
     def get(self):
-        a = request.args.get('a')
-        result = Decimal(a) * -1
+        a = Decimal(request.args.get('a'))
+        result = a.sqrt()
+        return {'result': float(result)}
+
+
+class Percent(Resource):
+
+    def get(self):
+        a = Decimal(request.args.get('a'))
+        b = Decimal(request.args.get('b'))
+        result = (b / 100) * a
         return {'result': float(result)}
 
 api.add_resource(Add, '/add')
 api.add_resource(Subtract, '/subtract')
 api.add_resource(Multiply, '/multiply')
 api.add_resource(Divide, '/divide')
-api.add_resource(Negative, '/negative')
+api.add_resource(SquareRoot, '/square_root')
+api.add_resource(Percent, '/percent')
 
 if __name__ == "__main__":
     if 'C9_USER' in os.environ:
--- static/calculator.js
+++ static/calculator.js
@@ -6,197 +6,279 @@ var result = '';
 
 $(document).ready(function() {
 	$(':button').click(function() {
-	    
-	    if ($(this).hasClass('num')) {
-	        if (lastInput.attr('id') == 'equ') {
-		        lastNum = 0;
-		        operator = '';
-		    }
+
+		if ($(this).hasClass('num')) {
+			if (lastInput.attr('id') == 'equ') {
+				lastNum = 0;
+				operator = '';
+			}
 			changeDisplay($(this).text());
-			
+
 			lastInput = $(this);
-			
+
 		} else if ($(this).hasClass('clr')) {
-		    lastNum = 0;
-		    operator = '';
-		    lastInput = $(this);
-		    changeDisplay('clr');
-		    
+			if ($(this).attr('id') == 'clr') {
+				lastNum = 0;
+				operator = '';
+				lastInput = $(this);
+			}
+			changeDisplay('clr');
+
 		} else if ($(this).hasClass('opr')) {
-		    result = '';
-		    switch ($(this).attr('id')) {
-		        case 'equ':
-		            switch (operator) {
-		                case '+':
-		                    result = add(lastNum, displayNum());
-	                        break;
-                        case '-':
-                            result = subtract(lastNum, displayNum());
-                            break;
-                        case 'x':
-                            result = multiply(lastNum, displayNum());
-                            break;
-                        case '/':
-                            result = divide(lastNum, displayNum());
-                            break;
-                        default:
-                            result = displayNum();
-	                }
-	                
-	                if (lastInput.attr('id') != 'equ') {
-	                    lastNum = displayNum();
-	                }
-		            break;
-		        
-		        case 'add':
-		            if (lastNum === 0 || lastInput.hasClass('opr')) {
-		                lastNum = displayNum();
-		            } else {
-		                result = add(lastNum, displayNum());
-		                lastNum = result;
-		            }
-		            operator = '+';
-		            break;
-		            
-                case 'sub':
-                    if (lastNum === 0 || lastInput.hasClass('opr')) {
-		                lastNum = displayNum();
-		            } else {
-		                result = subtract(lastNum, displayNum());
-		                lastNum = result;
-		            }
-		            operator = '-';
-		            break;
-	            
-	            case 'mul':
-                    if (lastNum === 0 || lastInput.hasClass('opr')) {
-		                lastNum = displayNum();
-		            } else {
-		                result = multiply(lastNum, displayNum());
-		                lastNum = result;
-		            }
-		            operator = 'x';
-		            break;
-		            
-	            case 'div':
-                    if (lastNum === 0 || lastInput.hasClass('opr')) {
-		                lastNum = displayNum();
-		            } else {
-		                result = divide(lastNum, displayNum());
-		                lastNum = result;
-		            }
-		            operator = '/';
-		            break;
-		            
-		        default:
-		            console.log('fix something');
-		            break;
-		    }
-		    
-		    lastInput = $(this);
-		    changeDisplay(result);
-		    
+			result = '';
+			switch ($(this).attr('id')) {
+				case 'equ':
+					switch (operator) {
+						case '+':
+							result = add(lastNum, displayNum());
+							break;
+						case '-':
+							result = subtract(lastNum, displayNum());
+							break;
+						case 'x':
+							result = multiply(lastNum, displayNum());
+							break;
+						case '/':
+							result = divide(lastNum, displayNum());
+							break;
+						default:
+							result = displayNum();
+					}
+
+					if (lastInput.attr('id') != 'equ') {
+						lastNum = displayNum();
+					}
+					break;
+
+				case 'add':
+					if (lastNum === 0 || lastInput.hasClass('opr')) {
+						lastNum = displayNum();
+					} else {
+						result = add(lastNum, displayNum());
+						lastNum = result;
+					}
+					operator = '+';
+					break;
+
+				case 'sub':
+					if (lastNum === 0 || lastInput.hasClass('opr')) {
+						lastNum = displayNum();
+					} else {
+						result = subtract(lastNum, displayNum());
+						lastNum = result;
+					}
+					operator = '-';
+					break;
+
+				case 'mul':
+					if (lastNum === 0 || lastInput.hasClass('opr')) {
+						lastNum = displayNum();
+					} else {
+						result = multiply(lastNum, displayNum());
+						lastNum = result;
+					}
+					operator = 'x';
+					break;
+
+				case 'div':
+					if (lastNum === 0 || lastInput.hasClass('opr')) {
+						lastNum = displayNum();
+					} else {
+						result = divide(lastNum, displayNum());
+						lastNum = result;
+					}
+					operator = '/';
+					break;
+
+				case 'sqr':
+					result = squareRoot(displayNum());
+					break;
+
+				case 'per':
+					result = percent(lastNum, displayNum());
+					break;
+
+				default:
+					console.log('fix something');
+					break;
+			}
+
+			lastInput = $(this);
+			changeDisplay(result);
+
 		} else if ($(this).hasClass('neg')) {
-		    changeDisplay('neg');
+			if (displayNum() !== 0 && !lastInput.hasClass('opr')) {
+				changeDisplay('neg');
+			}
+		} else if ($(this).hasClass('mem')) {
+			switch ($(this).attr('id')) {
+				case 'mcl':
+					memory = 0;
+					break;
+				case 'mrc':
+					if (memory !== 0) {
+						lastInput = $(this);
+						changeDisplay(memory);
+					}
+					break;
+				case 'mpl':
+					memory = add(memory, displayNum());
+					break;
+				case 'mmi':
+					memory = subtract(memory, displayNum());
+					break;
+			}
+
+			if (memory !== 0) {
+				$('#memory').css('visibility', 'visible');
+			} else {
+				$('#memory').css('visibility', 'hidden');
+			}
 		}
 	});
 });
 
 function displayNum() {
-    return +($('#display').text());
+	return +($('#display').text());
 }
 
 function displayTxt() {
-    return $('#display').text();
+	return $('#display').text();
 }
 
-function hasDecimal() {
-    if ($('#display').text().indexOf('.') === -1) {
-        return false;
-    }
-    return true;
+function hasDecimal(number) {
+	if (arguments.length == 1) {
+		text = number.toString();
+	} else {
+		text = $('#display').text();
+	}
+
+	if (text.indexOf('.') === -1) {
+		return false;
+	}
+	return true;
 }
 
-function isNegative() {
-    if ($('#display').text().indexOf('-') === 0) {
-        return true;
-    }
-    return false;
+function isNegative(number) {
+	if (arguments.length == 1) {
+		text = number.toString();
+	} else {
+		text = $('#display').text();
+	}
+
+	if (text.indexOf('-') === 0) {
+		return true;
+	}
+	return false;
 }
 
 function changeDisplay(input) {
-    input = input.toString();
-    var newDisp;
+	input = input.toString();
+	var newDisp;
 
-    if (input == 'clr') {
-        newDisp = '0';
-    } else if (input == '.') {
-        if (!hasDecimal()) {
-            newDisp = displayTxt() + input;
-        }
-    } else if (input == 'neg') {
-        if (isNegative()) {
-            newDisp = displayTxt().substring(1);
-        } else {
-            newDisp = '-' + displayTxt();
-        }
-    } else if (input.length && (displayNum() === 0 || lastInput.hasClass('opr'))) {
+	if (input == 'clr') {
+		newDisp = '0';
+	} else if (input == '.') {
+		if (!hasDecimal()) {
+			newDisp = displayTxt() + input;
+		}
+	} else if (input == 'neg') {
+		if (isNegative()) {
+			newDisp = displayTxt().substring(1);
+		} else {
+			newDisp = '-' + displayTxt();
+		}
+	} else if (input.length && (displayNum() === 0 || lastInput.hasClass('opr') || lastInput.hasClass('mem'))) {
 		newDisp = input;
 	} else {
 		newDisp = displayTxt() + input;
 	}
-	
-	$('#display').text(newDisp.substring(0,10));
+
+	var digits = 10;
+	if (isNegative(newDisp)) {
+		digits++;
+	}
+	if (hasDecimal(newDisp)) {
+		digits++;
+	}
+
+	$('#display').text(newDisp.substring(0, digits));
 }
 
 function add(a, b) {
-    var addResult = null;
+	var addResult = null;
 	$.ajax({
 		url: '/add',
 		data: {a: a, b: b},
 		async: false,
 		success: function(data) {
-		    addResult = data['result'];
+			addResult = data['result'];
 		}
 	});
 	return addResult;
 }
 
 function subtract(a, b) {
-    var subResult = null;
+	var subResult = null;
 	$.ajax({
 		url: '/subtract',
 		data: {a: a, b: b},
 		async: false,
 		success: function(data) {
-		    subResult = data['result'];
+			subResult = data['result'];
 		}
 	});
 	return subResult;
 }
 
 function multiply(a, b) {
-    var mulResult = null;
+	var mulResult = null;
 	$.ajax({
 		url: '/multiply',
 		data: {a: a, b: b},
 		async: false,
 		success: function(data) {
-		    mulResult = data['result'];
+			mulResult = data['result'];
 		}
 	});
 	return mulResult;
 }
 
 function divide(a, b) {
-    var divResult = null;
+	var divResult = null;
 	$.ajax({
 		url: '/divide',
 		data: {a: a, b: b},
 		async: false,
 		success: function(data) {
-		    divResult = data['result'];
+			divResult = data['result'];
 		}
 	});
 	return divResult;
+}
+
+function squareRoot(a) {
+	var sqrResult = null;
+	$.ajax({
+		url: '/square_root',
+		data: {a: a},
+		async: false,
+		success: function(data) {
+			sqrResult = data['result'];
+		}
+	});
+	return sqrResult;
+}
+
+function percent(a, b) {
+	var perResult = null;
+	$.ajax({
+		url: '/percent',
+		data: {a: a, b: b},
+		async: false,
+		success: function(data) {
+			perResult = data['result'];
+		}
+	});
+	return perResult;
 }
